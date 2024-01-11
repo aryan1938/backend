edb1835dac0dac8d4287a1907c3c9c556e812947
rtyuj <150962311+aryan1938@users.noreply.github.com>
2024-01-11 18:12:05 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const db =async () => {
-   await mongoose.connect('mongodb+srv://hacker:123@cluster0.g54zrui.mongodb.net/dark_forums').then(() => {
+   await mongoose.connect('mongodb+srv://hacker:123@cluster0.nguq63o.mongodb.net/dark_forums').then(() => {
         console.log("database connected");
     }).catch((err) => {
         console.log("Eror in Dattaase", err);
