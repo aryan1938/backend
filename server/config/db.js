6069e92fbf6fe419821836b6c38ef415dfe3a21a
rtyuj <150962311+aryan1938@users.noreply.github.com>
2024-01-11 18:02:27 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const db =async () => {
-   await mongoose.connect('mongodb://hacker:123@ac-zisgheu-shard-00-00.nguq63o.mongodb.net:27017,ac-zisgheu-shard-00-01.nguq63o.mongodb.net:27017,ac-zisgheu-shard-00-02.nguq63o.mongodb.net:27017/?ssl=true&replicaSet=atlas-po1wpv-shard-0&authSource=admin&retryWrites=true&w=majority/dark_forums').then(() => {
+   await mongoose.connect('mongodb+srv://hacker:123@cluster0.g54zrui.mongodb.net/dark_forums').then(() => {
         console.log("database connected");
     }).catch((err) => {
         console.log("Eror in Dattaase", err);
